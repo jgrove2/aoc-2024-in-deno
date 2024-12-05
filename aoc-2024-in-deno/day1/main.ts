b74b322f1b66078fd0d275abcdfa0289c2a207f6
jgrove <26723005+jgrove2@users.noreply.github.com>
2024-12-05 03:11:25 +0000

    day4

--- aoc-2024-in-deno/day1/main.ts
+++ aoc-2024-in-deno/day1/main.ts
@@ -2,25 +2,25 @@ const example1 = await Deno.readTextFile("./day1/example1.txt");
 const input = await Deno.readTextFile("./day1/input.txt");
 
 function getDataFromList(data: string) {
-  const regex = /(\d+)\s+(\d+)/
+  const regex = /(\d+)\s+(\d+)/;
   const leftList = [];
   const rightList = [];
   data.split("\n").forEach((line: string) => {
     const match = line.match(regex);
-    if(match) {
+    if (match) {
       leftList.push(parseInt(match[1]));
       rightList.push(parseInt(match[2]));
     }
-  })
+  });
   return [leftList, rightList];
 }
 
 function part1() {
-  const list = getDataFromList(input)
-  const rightList = list[1].sort()
-  const leftList = list[0].sort()
+  const list = getDataFromList(input);
+  const rightList = list[1].sort();
+  const leftList = list[0].sort();
   let total = 0;
-  for(let i = 0; i < rightList.length; i++) {
+  for (let i = 0; i < rightList.length; i++) {
     total += Math.abs(rightList[i] - leftList[i]);
   }
   console.log(total);
@@ -28,18 +28,18 @@ function part1() {
 
 function part2() {
   const list = getDataFromList(input);
-  const rightList = list[1]
-  const leftList = list[0]
-  let mapList = {}
+  const rightList = list[1];
+  const leftList = list[0];
+  let mapList = {};
   rightList.forEach((r) => {
     mapList[r] = (mapList[r] || 0) + 1;
-  })
+  });
   let total = leftList.reduce((acc, e) => {
     let ct = mapList[e] || 0;
-    return acc + (ct * e)
-  }, 0)
-  console.log(total)
+    return acc + (ct * e);
+  }, 0);
+  console.log(total);
 }
 
-part1()
-part2()
+part1();
+part2();
--- aoc-2024-in-deno/day2/main.ts
+++ aoc-2024-in-deno/day2/main.ts
@@ -2,40 +2,40 @@ const example1 = await Deno.readTextFile("./day2/example1.txt");
 const input = await Deno.readTextFile("./day2/input1.txt");
 
 function part1() {
-  let rows = rowsFromText(input)
+  let rows = rowsFromText(input);
   let safeCount = 0;
   rows.forEach((row) => {
-    if(isSafe(row)) safeCount += 1
-  })
-  console.log(safeCount)
+    if (isSafe(row)) safeCount += 1;
+  });
+  console.log(safeCount);
 }
 
 function part2() {
-  let rows = rowsFromText(input)
-  let safeCount = 0
+  let rows = rowsFromText(input);
+  let safeCount = 0;
   rows.forEach((row) => {
     let safe = false;
-    for(let i = 0; i < row.length; i ++ ) {
-      let c = row.slice(0, i).concat(row.slice(i+1))
-      if(isSafe(c)) {
+    for (let i = 0; i < row.length; i++) {
+      let c = row.slice(0, i).concat(row.slice(i + 1));
+      if (isSafe(c)) {
         safe = true;
         break;
       }
-    } 
-    if(isSafe(row) || safe) safeCount ++;
-  })
-  console.log(safeCount)
+    }
+    if (isSafe(row) || safe) safeCount++;
+  });
+  console.log(safeCount);
 }
 
 function rowsFromText(text: string) {
-  return text.split("\n").map((arr) => arr.split(' ')).slice(0,-1)
+  return text.split("\n").map((arr) => arr.split(" ")).slice(0, -1);
 }
 
 function isSafe(row: number[]) {
   let accending = row[0] > row[1];
-  if(row[0] === row[1]) return false;
-  for(let i = 0; i < row.length - 1; i++) {
-    if(!isWithinThree(row[i], row[i+1], accending)){
+  if (row[0] === row[1]) return false;
+  for (let i = 0; i < row.length - 1; i++) {
+    if (!isWithinThree(row[i], row[i + 1], accending)) {
       return false;
     }
   }
@@ -43,10 +43,10 @@ function isSafe(row: number[]) {
 }
 
 function isWithinThree(num1: number, num2: number, accending: boolean) {
-  if(accending) {
-    return num1-num2 <= 3 && num1-num2 > 0;
+  if (accending) {
+    return num1 - num2 <= 3 && num1 - num2 > 0;
   }
-  return num1-num2 >= -3 && num1-num2 < 0
+  return num1 - num2 >= -3 && num1 - num2 < 0;
 }
 
 part1();
